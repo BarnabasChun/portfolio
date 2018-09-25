3a3ae4defcb51447b2ce1ade3b1fc4f874124c0a
Barnabas Chun <chun.barnabas@gmail.com>
2018-09-25 12:40:24 -0400

    Make fixed nav background darker

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const navHeight = headerNav.height();
 
 $(window).scroll(() => {
   if (window.scrollY > navHeight) {
-    headerNav.css('background-color', 'rgba(0, 0, 0, .5)');
+    headerNav.css('background-color', 'rgba(0, 0, 0, .85)');
   } else {
     headerNav.css('background-color', 'transparent');
   }
